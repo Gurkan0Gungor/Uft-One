277d7fd4313acc413848b7775a44f1718c3ecb24
Gurkan0Gungor <gurkan.gungor@virgosol.com>
2023-04-07 11:16:26 +0300

    first commit

---
+++ Res28/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\Hp\\Documents\\UFT One\\Deneme\\Res28',namePairs:[]}]};
---
+++ Res74/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\Virgosol\\Desktop\\UFTPROJECT\\Deneme\\Res74',namePairs:[]}]};
